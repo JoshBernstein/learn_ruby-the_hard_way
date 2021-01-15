5ef2ef76a03d1059e81812899c4c18d7326ea3b2
JoshBernstein <josh.bern@gmail.com>
2021-01-15 14:29:33 -0500

    ex10-12

---
+++ ex12.rb
@@ -0,0 +1,16 @@
+# print "Give me a number: "
+# number = gets.chomp.to_i
+
+# bigger = number * 100
+# puts "A bigger number is #{bigger}."
+
+# print "Give me another number: "
+# another = gets.chomp
+# number = another.to_f
+
+# smaller = number / 100
+# puts "A smaller number is #{smaller}."
+
+puts "How much money would you like to give me?"
+money = gets.chomp.to_f
+puts "Thank you. Your change is #{money / 10}."
